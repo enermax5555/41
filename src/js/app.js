787472c4dbd506e7983904d531c972be55f6144c
Стоян Калпакчиев <stoqn.sisimen@gmail.com>
2021-12-01 11:30:20 -0300

    Fetch the api and finish the task

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+	fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+	.then(r => r.json())
+	.then(r => {
+		pokemons.forEach(pokemon => {
+			let list = document.createElement('li');
+			list.innerText = pokemon.name;
+			ul.appendChild(list);
+		})
+	})
 });
